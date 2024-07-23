87470c4cd74d4afa6b63bd6766ac2b950d1a8eb8
Fábio Henrique <fabio.pauleli@gmail.com>
2024-07-22 21:52:19 -0300

    Adiciona exemplo de uso da função input em Python
    Demonstra como utilizar a função input para coletar dados do usuário durante a execução do programa. Este exemplo inclui a solicitação de um nome e armazena o valor fornecido em uma variável para uso posterior.
    
    Adds example of using the input function in Python
    
    Shows how to use the input function to collect user data during program execution. This example includes prompting for a name and storing the provided value in a variable for later use.

---
+++ aula15.py
@@ -0,0 +1,10 @@
+# Aula de como coletar dados utilizando input.
+# nome = input('Qual o seu nome? ')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ') #Evitar fazer a conversão do tipo de dado direto na variavel.
+
+int_numero_1 = int(numero_1) #Fazer a conversão do tipo de dado em outra variavel é o ideal para conseguirmos verificar o dado antes de prosseguir.
+int_numero_2 = int(numero_2) #Exemplo é colocar uma checagem antes de somar, para validar se não é uma string.
+
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
