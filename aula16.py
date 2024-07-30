546a109104883da546eefc5f2dbefceb0526726f
Fábio Henrique <fabio.pauleli@gmail.com>
2024-07-29 23:04:13 -0300

    Learning about block structures, using if, elif, and else Aprendendo sobre estruturas de bloco, utilizando if, elif e else

--- aula16.py
+++ aula16.py
@@ -1,1 +1,11 @@
-f
+# if / elif      / else
+# se / se não se / se não
+
+entrada = input('Você quer "Entrar" ou "Sair"? ')
+
+if entrada == 'Entrar':
+    print('Você entrou no sistema')
+elif entrada == 'Sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou nem entrar e nem sair.')
