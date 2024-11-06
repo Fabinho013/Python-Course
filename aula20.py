ec6a337b7b82c499d367790dc84489c654023db1
Fábio Henrique <fabio.pauleli@gmail.com>
2024-11-06 00:50:34 -0300

    Adds input example using logical operators
    Adiciona exemplo de uso do input com operadores lógicos

---
+++ aula20.py
@@ -0,0 +1,12 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# and - Todas as condições precisam ser verdadeiras.
+# Se qualquer valor for considerado falso, a expressão inteira será avaliada naquele valor
+# São considerados falsy: o Zero (0), 0.0 ou False
+# Também existe o tipo de dado chamado None que é usado para representar um não valor    
+
+entrada = input('[E]ntrar [S]air: ')
+
+if Entrada == 'E':
+    print('Entrar')
+el
