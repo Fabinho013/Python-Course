14e114962da6a1ba1192beadc15d60574911b254
Fabinho013 <fabio.pauleli@gmail.com>
2025-04-22 02:28:31 -0300

    Tipos built-in, tipos imutáveis e métodos de string / Built-in types, immutables and string methods

---
+++ aula33.py
@@ -0,0 +1,26 @@
+# Tipos built-in, documentação, tipos imutáveis, métodos de string /
+# Built-in types, documentation, immutable types, string methods
+
+"""
+Documentação oficial sobre tipos embutidos (built-in types):
+https://docs.python.org/pt-br/3/library/stdtypes.html
+
+Tipos imutáveis que vimos: str, int, float, bool
+"""
+
+# Definição de uma string (imutável)
+string = 'Fábio Henrique'
+
+# Exemplo de método capitalize(), que transforma a primeira letra em maiúscula
+# e todas as outras em minúsculas
+print(string.capitalize())
+
+# Exemplo comentado:
+# Tentativa de alterar diretamente um caractere da string (não é permitido, pois str é imutável)
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# string[4] = 'ABCDE'  # Isso geraria um erro
+
+# print(outra_variavel)
+
+# Aula sobre tipos embutidos, documentação, tipos imutáveis e métodos de string /
+# Lesson on built-in types, documentation, immutable types, and string methods
