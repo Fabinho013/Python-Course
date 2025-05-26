67a3b5c38acb4fb259ad82a598ee6e79e38e05a6
Fabinho013 <fabio.pauleli@gmail.com>
2025-05-25 23:18:08 -0300

    Aula 44 - [Aula sobre laços aninhados com while / Lesson on nested loops with while]

---
+++ aulas/aula44.py
@@ -0,0 +1,9 @@
+# Aula 44 - Aula sobre o uso de for com a função range para iteração controlada / Lesson on using for with the range function for controlled iteration
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
