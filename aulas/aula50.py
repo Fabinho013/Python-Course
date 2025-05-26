a978a384b17f8de8e8abcfc15e5d9817fb9bc70d
Fabinho013 <fabio.pauleli@gmail.com>
2025-05-25 23:18:13 -0300

    Aula 50 - [Métodos de modificação em listas: append, pop, insert, del e clear / List modification methods: append, pop, insert, del, and clear]

---
+++ aulas/aula50.py
@@ -0,0 +1,26 @@
+# Aula 50 - Métodos de modificação em listas: append, pop, insert, del e clear / List modification methods: append, pop, insert, del, and clear
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3
+lista = [10, 20, 30, 40]
+lista.append('Luiz')
+nome = lista.pop()
+lista.append(1233)
+del lista[-1]
+# lista.clear()
+lista.insert(100, 5)
+print(lista[4])
