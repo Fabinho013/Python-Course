ed0134fa294bc05617b65530d8a0a61f6c2ef760
Fabinho013 <fabio.pauleli@gmail.com>
2025-06-02 17:59:38 -0300

    Aula 54 - [Aula sobre Tipo tupla: uma lista imutável / Tuple type: an immutable list]

---
+++ aulas/aula54.py
@@ -0,0 +1,9 @@
+# Aula 54 - Tipo tupla: uma lista imutável / Tuple type: an immutable list
+"""
+Tipo tupla - Uma lista imutável
+"""
+nomes = ('Maria', 'Helena', 'Luiz')
+# nomes = tuple(nomes)
+# nomes = list(nomes)
+print(nomes[-1])
+print(nomes)
