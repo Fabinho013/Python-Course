fa44696b01a8f1c7f5499488ef4a7d9299876efb
Fabinho013 <fabio.pauleli@gmail.com>
2025-06-02 17:59:37 -0300

    Aula 52 - [Aula sobre Laço for com listas / Looping through lists with for]

---
+++ aulas/aula52.py
@@ -0,0 +1,9 @@
+# Aula 44 - Laço for com listas / Looping through lists with for
+
+"""
+for in com listas
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+
+for nome in lista:
+    print(nome, type(nome))
